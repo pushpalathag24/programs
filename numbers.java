8c990c583d6f0ac156583dcac0e67cb37068b2f3
pushpalathag24 <pushpalathag24cse@vetias.ac.in>
2025-08-19 12:08:40 +0530

    finished the program

--- numbers.java
+++ numbers.java
@@ -2,8 +2,7 @@ public class numbers{
     public static void main(String[] args){
         int num1=2;
         int num2=4;
-        int =num1+num2;
+        int a=num1+num2;
+        System.out.println(a);
     }
 }
-
-random
